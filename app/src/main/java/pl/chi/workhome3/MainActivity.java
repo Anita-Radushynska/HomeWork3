dc416c8c95b5998ff22a1c3e2434431963a9a8f9
Anita-Radushynska <anita.radushynska@gmail.com>
2020-12-17 19:27:55 +0100

    first commit

--- app/src/main/java/pl/chi/workhome3/MainActivity.java
+++ app/src/main/java/pl/chi/workhome3/MainActivity.java
@@ -2,17 +2,49 @@ package pl.chi.workhome3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.ImageView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    Button btn;
+    ImageButton imgBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ImageView myImageView = (ImageView) findViewById(R.id.my_image_view);
-        myImageView.setImageResource(R.drawable.th);
+        btn = findViewById(R.id.btn);
+        imgBtn = findViewById(R.id.ImgBtn);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showToast("Toast appered");
+            }
+        });
+
+        imgBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openNewActivity();
+            }
+        });
+
     }
+
+    public void showToast (String message){
+    Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+    }
+
+    public void openNewActivity(){
+        Intent intent = new Intent(this, CustomActivity.class);
+        startActivity(intent);
+    }
+
 }
